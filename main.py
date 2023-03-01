3f20a3d548da035ad578a9c916c98d76bbea41da
kudrovo32 <126150306+kudrovo32@users.noreply.github.com>
2023-03-01 11:06:59 +1100

    modified functions

--- main.py
+++ main.py
@@ -37,7 +37,6 @@ client = Client() #bot initialisation
 async def on_ready():
     print(f"We have logged in as {client.user}.")
     
-
 #message command to sync slash commands
 @client.command()
 @commands.guild_only()
@@ -49,10 +48,10 @@ async def sync(ctx: Context):
 #@client.event
 #async def on_message(message):
         #if message.author == client.user:
-        #     return 
+         #    return 
 
         #if message.author.id == XENON:
-        #    await message.channel.send('Haha?')
+         #   await message.channel.send('Haha?')
 
 
 #@client.tree.command(name = "gpt", description = "prompt ChatGPT")
@@ -92,31 +91,42 @@ async def ety_tree(interaction: discord.Interaction, given_word: str):
 #scripted_text
 scripted_text = list(['usedtah have a buddie down in baltimore he never heard of an pork roll egg n cheese. wiseguy.','my daughtah cindy got a new boyfriend. maddonee.','it could never take you five minutes to cook your grits, it takes the entire grit-eating world 20 minutes.','nothing will topa good pizzzah with led zep playing at the maddie square','You should try out mah pizzahhh! iz good', 'you call it a pizzzahh i call it a work of michelangelo. and im the one that made it', 'they tell me you gonna be something one day well. ohh madone.', 'my wife marie makes the best cupcakes','you need a sitdown my friend','you ever tasted a pizzzah from detroit? no wonder', 'God with a capital G, wiseguy', 'ohh madone.','what you call a doctor with three heads? probably by his name', 'there were three of them. a cheerleader, a broad with an expiry date of three yeasz, and my nephew who dont hear too good. who u pickin to make the best pizza in the penn? ME.', 'you know who make the best pizza in the penn dont yous', 'you ever heard of a machine who pics up all the rotten apples off the dirt? yeah me neither', 'whats new jersey got tah do with it', 'only one of us is making it out and im the best pizza maker in the penn', 'you ever seen heat 1995 film yeah itzza my favorite.', 'dont ever take gods name in vain', 'a chessboard with two kings. you call that a stalemate. i call that a good time', 'you want to hear three jokes well its only a matter of time before they call you up on stage', 'i coulda been a contender', 'you ever been to a lodge near the ocean yeah my great great great grandparents had one in napoli'])
 
-#provides the number of sleeps until christmas with a timezone input
-@client.tree.command(name = "christmas_sleeps", description = "Provides the number of sleeps until Christmas. Requires timezone input")
+#Provides the number of sleeps until a certain few special days. Requires timezone input
+@client.tree.command(name = "sleeps_to_holiday", description = "Provides the number of sleeps until a certain few special days. Requires timezone input")
 @app_commands.guilds(discord.Object(GUILD_ID))
-async def christmas_sleeps(interaction: discord.Interaction, chosen_timezone: str):
+async def sleeps_to_holiday(interaction: discord.Interaction, chosen_holiday: str, chosen_timezone: str):
     timezone = pytz.timezone(chosen_timezone)
     
-    christmas_day = datetime.datetime(2023, 12, 25, 7, 0, 0, 0, timezone)
     current_time = datetime.datetime.now(timezone)
-    
     timezone_name = current_time.tzname()
 
-    time_diff = christmas_day - current_time
+    special_day = datetime.datetime.fromisoformat(chosen_holiday)
+    #christmas_day_aware = christmas_day.replace(tzinfo=timezone.timezone_name)
+    special_day_aware = timezone.localize(special_day)
+
+    time_diff = special_day_aware - current_time
     sleeps = time_diff.days
 
     if current_time.hour >= 18:
         sleeps -= 1
 
+    holiday_name = None
+    if chosen_holiday == '2023-12-25T07:00:00':
+        holiday_name = 'normal Christmas'
+    if chosen_holiday == '2024-01-07T07:00:00':
+        holiday_name = 'Serbian Christmas'
+    if chosen_holiday == '2023-11-12T07:00:00':
+        holiday_name = 'National Pizza With Everything (Except Anchovies)'
+
+
     random_int = random.randint(0,len(scripted_text)-1)
     chosen_text = scripted_text[random_int]
 
-    await interaction.response.send_message(f"{chosen_text} {interaction.user.mention} that's how i know there are {sleeps} sleeps until Christmas day in the timezone region of {timezone} ({timezone_name}).")
+    await interaction.response.send_message(f"{chosen_text} {interaction.user.mention} that's how i know there are {sleeps} sleeps until {holiday_name} day in the timezone region of {timezone} ({timezone_name}).")
 
 #defines choices in previous function
-@christmas_sleeps.autocomplete("chosen_timezone")
-async def christmas_sleeps_timezone_autocompletion(
+@sleeps_to_holiday.autocomplete("chosen_timezone")
+async def sleeps_to_holiday_timezone_autocompletion(
     interaction: discord.Interaction, 
     current: str
     ) -> List[app_commands.Choice[str]]:
@@ -124,6 +134,17 @@ async def christmas_sleeps_timezone_autocompletion(
         data = []
         for timezone_choice in picked_timezones:
             data.append(app_commands.Choice(name=timezone_choice, value = timezone_choice))
+        return data
+
+@sleeps_to_holiday.autocomplete("chosen_holiday")
+async def chosen_holiday_autocompletion(
+    interaction: discord.Interaction, 
+    current: str
+    ) -> List[app_commands.Choice]:
+        data = []
+        data.append(app_commands.Choice(name='Normal Christmas', value ='2023-12-25T07:00:00'))
+        data.append(app_commands.Choice(name='Serbian Christmas', value ='2024-01-07T07:00:00'))
+        data.append(app_commands.Choice(name='National Pizza With Everything (Except Anchovies) Day', value='2023-11-12T07:00:00'))
         return data
 
 client.run(CLIENT_TOKEN) #run with token
